b0f76a1d98ae4a6a31bdc89ef52ff664c84b3588
Nancharla Prudhvi <prudhvinancharla@gmail.com>
2026-09-24 23:02:38 +0530

    Integrate TS Treasury pensioners database, display only Old DR, New DR and DR Difference by PPO ID

---
+++ pensioners_db.js
@@ -0,0 +1,530 @@
+﻿// ========================================================
+// TS PENSIONERS VERIFIED DATABASE (TELANGANA TREASURY RECORDS)
+// Formulated & Integrated by NANCHARLA PRUDHVI
+// Senior Accountant, District Treasury, Khammam
+// ========================================================
+window.PENSIONERS_DATABASE = 
+{
+    "01001013":  {
+                     "basic":  20190,
+                     "name":  "GANGAMMA K"
+                 },
+    "01001047":  {
+                     "basic":  52215,
+                     "name":  "YADAGIRI K"
+                 },
+    "01001149":  {
+                     "basic":  32785,
+                     "name":  "BHOODEVI"
+                 },
+    "01001138":  {
+                     "basic":  54875,
+                     "name":  "SHANKAR RAO S"
+                 },
+    "01001073":  {
+                     "basic":  54875,
+                     "name":  "PURNIMA DEVI"
+                 },
+    "01001091":  {
+                     "basic":  40480,
+                     "name":  "MOHD ABDUL RAZAK"
+                 },
+    "01001062":  {
+                     "basic":  35500,
+                     "name":  "CHILUKA LACHANNA"
+                 },
+    "01001050":  {
+                     "basic":  50935,
+                     "name":  "SRIDHAR M"
+                 },
+    "01001009":  {
+                     "basic":  37958,
+                     "name":  "BHEEVA K"
+                 },
+    "01001068":  {
+                     "basic":  57349,
+                     "name":  "DURGA BHAVANI M K"
+                 },
+    "01001004":  {
+                     "basic":  15571,
+                     "name":  "DADE RAO M"
+                 },
+    "01001065":  {
+                     "basic":  21300,
+                     "name":  "K RUKMINI"
+                 },
+    "01001040":  {
+                     "basic":  52215,
+                     "name":  "SAI PURNA CHANDER S"
+                 },
+    "01001007":  {
+                     "basic":  25334,
+                     "name":  "ASHOK REDDY K"
+                 },
+    "01001118":  {
+                     "basic":  46880,
+                     "name":  "SANTOSHKUMAR K"
+                 },
+    "01001014":  {
+                     "basic":  47398,
+                     "name":  "VAZRAMMA P"
+                 },
+    "01001130":  {
+                     "basic":  50935,
+                     "name":  "JANARDHAN GOUD A"
+                 },
+    "01001017":  {
+                     "basic":  35500,
+                     "name":  "SYED HASAN ALI"
+                 },
+    "01001116":  {
+                     "basic":  53495,
+                     "name":  "VENKATESHWARLU K"
+                 },
+    "01001139":  {
+                     "basic":  16900,
+                     "name":  "KOTA VIJAYA"
+                 },
+    "01001086":  {
+                     "basic":  45903,
+                     "name":  "CHINNAMMA S"
+                 },
+    "01001140":  {
+                     "basic":  33650,
+                     "name":  "B NARSIMULU"
+                 },
+    "01001096":  {
+                     "basic":  49655,
+                     "name":  "BALIRAM J"
+                 },
+    "01001121":  {
+                     "basic":  66815,
+                     "name":  "RAJANNA S"
+                 },
+    "01001099":  {
+                     "basic":  34575,
+                     "name":  "KISHAN G"
+                 },
+    "01001076":  {
+                     "basic":  20619,
+                     "name":  "SHOBHA RANI G"
+                 },
+    "01001036":  {
+                     "basic":  46025,
+                     "name":  "LAXMAN M"
+                 },
+    "01001056":  {
+                     "basic":  48445,
+                     "name":  "LAXMIKANTHAM M"
+                 },
+    "01001051":  {
+                     "basic":  29793,
+                     "name":  "CHANDRAMOHAN N"
+                 },
+    "01001124":  {
+                     "basic":  35500,
+                     "name":  "R SEETHARAM"
+                 },
+    "01001061":  {
+                     "basic":  35500,
+                     "name":  "R RAMA RAO"
+                 },
+    "01001038":  {
+                     "basic":  54875,
+                     "name":  "PREM SINGH C"
+                 },
+    "01001003":  {
+                     "basic":  48445,
+                     "name":  "DASHARATH A"
+                 },
+    "01001148":  {
+                     "basic":  37420,
+                     "name":  "K.RAMESH"
+                 },
+    "01001106":  {
+                     "basic":  54875,
+                     "name":  "M A RASHEED"
+                 },
+    "01001109":  {
+                     "basic":  33650,
+                     "name":  "P SHALIK RAO"
+                 },
+    "01001131":  {
+                     "basic":  47235,
+                     "name":  "YASHWANTH RAO R"
+                 },
+    "01001070":  {
+                     "basic":  50935,
+                     "name":  "RAMJEE A"
+                 },
+    "01001147":  {
+                     "basic":  28065,
+                     "name":  "R.GOVIND"
+                 },
+    "01001075":  {
+                     "basic":  59115,
+                     "name":  "ISHRATH PARVEEN"
+                 },
+    "01001085":  {
+                     "basic":  41550,
+                     "name":  "PUSPALATHA A"
+                 },
+    "01001059":  {
+                     "basic":  50935,
+                     "name":  "TUKARAM J"
+                 },
+    "01001055":  {
+                     "basic":  11667,
+                     "name":  "SHOBHA M"
+                 },
+    "01001069":  {
+                     "basic":  68525,
+                     "name":  "SUKEERTHA M"
+                 },
+    "01001054":  {
+                     "basic":  52215,
+                     "name":  "VEERAIAH G"
+                 },
+    "01001066":  {
+                     "basic":  39410,
+                     "name":  "G SONA RAO"
+                 },
+    "01001057":  {
+                     "basic":  50935,
+                     "name":  "NARENDER S"
+                 },
+    "01001006":  {
+                     "basic":  46025,
+                     "name":  "JAGAN MOHAN REDDY"
+                 },
+    "01001043":  {
+                     "basic":  12081,
+                     "name":  "KISTA BAI S"
+                 },
+    "01001141":  {
+                     "basic":  34575,
+                     "name":  "MD NIZAMUDDIN"
+                 },
+    "01001028":  {
+                     "basic":  20190,
+                     "name":  "ASMA BEGUM"
+                 },
+    "01001128":  {
+                     "basic":  54875,
+                     "name":  "DAMODAR RAJU B"
+                 },
+    "01001084":  {
+                     "basic":  50935,
+                     "name":  "RAMESH CHANDER R"
+                 },
+    "01001108":  {
+                     "basic":  32785,
+                     "name":  "S LAXMAN"
+                 },
+    "01001125":  {
+                     "basic":  12414,
+                     "name":  "HASEENA BANU"
+                 },
+    "01001063":  {
+                     "basic":  28980,
+                     "name":  "B KISHAN"
+                 },
+    "01001095":  {
+                     "basic":  44890,
+                     "name":  "JAGAN A"
+                 },
+    "01001122":  {
+                     "basic":  52215,
+                     "name":  "TELANGA RAO K"
+                 },
+    "01001113":  {
+                     "basic":  27501,
+                     "name":  "SHAIK SAMEENA SULTANA"
+                 },
+    "01001098":  {
+                     "basic":  49655,
+                     "name":  "DHAVJI RATHOD"
+                 },
+    "01001060":  {
+                     "basic":  48445,
+                     "name":  "NARSING RAO D"
+                 },
+    "01001103":  {
+                     "basic":  47235,
+                     "name":  "RAMA SWAMY R"
+                 },
+    "01001074":  {
+                     "basic":  44890,
+                     "name":  "JAYASRI A"
+                 },
+    "01001034":  {
+                     "basic":  53495,
+                     "name":  "GANGAIAH  CH"
+                 },
+    "01001026":  {
+                     "basic":  16623,
+                     "name":  "VIJAYASRI P"
+                 },
+    "01001053":  {
+                     "basic":  49655,
+                     "name":  "VENKAT RAO K"
+                 },
+    "01001102":  {
+                     "basic":  46025,
+                     "name":  "YELLANNA S"
+                 },
+    "01001150":  {
+                     "basic":  32785,
+                     "name":  "T.SUNDARI BAI"
+                 },
+    "01001105":  {
+                     "basic":  57635,
+                     "name":  "LAXMIKANTH RAO A"
+                 },
+    "01001044":  {
+                     "basic":  31055,
+                     "name":  "GANGA REDDY T"
+                 },
+    "01001142":  {
+                     "basic":  35500,
+                     "name":  "ABDUL RASHEED"
+                 },
+    "01001072":  {
+                     "basic":  52215,
+                     "name":  "VASANTHI G"
+                 },
+    "01001071":  {
+                     "basic":  36425,
+                     "name":  "SUNITHA G"
+                 },
+    "01001094":  {
+                     "basic":  31055,
+                     "name":  "K SONE RAO"
+                 },
+    "01001101":  {
+                     "basic":  59115,
+                     "name":  "VENU MADURI"
+                 },
+    "01001058":  {
+                     "basic":  68525,
+                     "name":  "MOHAN RAO R"
+                 },
+    "01001087":  {
+                     "basic":  44890,
+                     "name":  "ANAND RAO M"
+                 },
+    "01001033":  {
+                     "basic":  56255,
+                     "name":  "LAXMI NARASIMHA MURTHY"
+                 },
+    "01001088":  {
+                     "basic":  48445,
+                     "name":  "VITTAL RAO M"
+                 },
+    "01001067":  {
+                     "basic":  41550,
+                     "name":  "RAMESH REDDY K"
+                 },
+    "01001027":  {
+                     "basic":  59115,
+                     "name":  "KALPANA P"
+                 },
+    "01001005":  {
+                     "basic":  43755,
+                     "name":  "ASHOK D"
+                 },
+    "01001024":  {
+                     "basic":  28980,
+                     "name":  "K ISTARI"
+                 },
+    "01001010":  {
+                     "basic":  19152,
+                     "name":  "BODAM KUNTI LAXMI BAI"
+                 },
+    "01001119":  {
+                     "basic":  48445,
+                     "name":  "PRATHIBHA G"
+                 },
+    "01001029":  {
+                     "basic":  32785,
+                     "name":  "INAMULLAH KHAN"
+                 },
+    "01001114":  {
+                     "basic":  11439,
+                     "name":  "ASHAMOLLA DEVUBAI"
+                 },
+    "01001145":  {
+                     "basic":  43755,
+                     "name":  "BHUMANNA KODAPA"
+                 },
+    "01001129":  {
+                     "basic":  77345,
+                     "name":  "SRINIVAS GOUD P"
+                 },
+    "01001083":  {
+                     "basic":  40480,
+                     "name":  "RAJENDER D"
+                 },
+    "01001112":  {
+                     "basic":  32785,
+                     "name":  "LAXMI BAI"
+                 },
+    "01001144":  {
+                     "basic":  59115,
+                     "name":  "DAVID YESUDAS B"
+                 },
+    "01001037":  {
+                     "basic":  68525,
+                     "name":  "NAGA REDDY G"
+                 },
+    "01001136":  {
+                     "basic":  44890,
+                     "name":  "MOHD ISHAQUE"
+                 },
+    "01001081":  {
+                     "basic":  16117,
+                     "name":  "RAZIYA BEGUM"
+                 },
+    "01001133":  {
+                     "basic":  49655,
+                     "name":  "SHIVA REDDY D"
+                 },
+    "01001039":  {
+                     "basic":  38352,
+                     "name":  "NAFEES FATHIMA"
+                 },
+    "01001048":  {
+                     "basic":  11761,
+                     "name":  "ANJUM SHABANA"
+                 },
+    "01001011":  {
+                     "basic":  49655,
+                     "name":  "SHANKER G"
+                 },
+    "01001090":  {
+                     "basic":  31055,
+                     "name":  "ANJANI BAI V"
+                 },
+    "01001045":  {
+                     "basic":  59115,
+                     "name":  "GANGA REDDY K"
+                 },
+    "01001093":  {
+                     "basic":  27138,
+                     "name":  "G MADHUKAR"
+                 },
+    "01001046":  {
+                     "basic":  29324,
+                     "name":  "SYED ANSAR RAZA"
+                 },
+    "01001092":  {
+                     "basic":  33650,
+                     "name":  "KISHAN SINGH M"
+                 },
+    "01001082":  {
+                     "basic":  12174,
+                     "name":  "MOHAMMAD MOQEER UDDIN"
+                 },
+    "01001120":  {
+                     "basic":  41550,
+                     "name":  "MADHUKAR V"
+                 },
+    "01001100":  {
+                     "basic":  47235,
+                     "name":  "SUDHAKAR H"
+                 },
+    "01001016":  {
+                     "basic":  50935,
+                     "name":  "ABDULLAH KHAN"
+                 },
+    "01001032":  {
+                     "basic":  53495,
+                     "name":  "RAMANA G"
+                 },
+    "01001089":  {
+                     "basic":  50935,
+                     "name":  "NARSING RAO R"
+                 },
+    "01001031":  {
+                     "basic":  18633,
+                     "name":  "LAYAKH BEGUM"
+                 },
+    "01001015":  {
+                     "basic":  44890,
+                     "name":  "NARENDER RAO S"
+                 },
+    "01001135":  {
+                     "basic":  52215,
+                     "name":  "M ASHOK"
+                 },
+    "01001025":  {
+                     "basic":  54875,
+                     "name":  "PEDDA BAPU"
+                 },
+    "01001042":  {
+                     "basic":  23133,
+                     "name":  "SURYA KALA P"
+                 },
+    "01001115":  {
+                     "basic":  33650,
+                     "name":  "CH PRABHAKAR"
+                 },
+    "01001137":  {
+                     "basic":  53495,
+                     "name":  "WASEEM ANSARI"
+                 },
+    "01001078":  {
+                     "basic":  33650,
+                     "name":  "LALU G"
+                 },
+    "01001110":  {
+                     "basic":  35960,
+                     "name":  "B SATYANARAYANA"
+                 },
+    "01001146":  {
+                     "basic":  52215,
+                     "name":  "SUDHAKAR S"
+                 },
+    "01001035":  {
+                     "basic":  31920,
+                     "name":  "RAJANNA"
+                 },
+    "01001052":  {
+                     "basic":  47235,
+                     "name":  "SHESHA RAO R"
+                 },
+    "01001001":  {
+                     "basic":  32785,
+                     "name":  "ABDUL SULTAN"
+                 },
+    "01001030":  {
+                     "basic":  41550,
+                     "name":  "T MADHAV RAO"
+                 },
+    "01001104":  {
+                     "basic":  14170,
+                     "name":  "VACHALA BAI S"
+                 },
+    "01001107":  {
+                     "basic":  38415,
+                     "name":  "SHESHNARAYAN MESRAM"
+                 },
+    "01001123":  {
+                     "basic":  36425,
+                     "name":  "J RAMCHANDER"
+                 },
+    "01001049":  {
+                     "basic":  40480,
+                     "name":  "BHUMANNA B"
+                 },
+    "01001134":  {
+                     "basic":  50935,
+                     "name":  "NARAYAN"
+                 },
+    "01001022":  {
+                     "basic":  19152,
+                     "name":  "CHILURI KRISHNAVENI"
+                 }
+}
+;
+
